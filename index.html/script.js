2a883868103337761b9064210ac02b28c99cea46
thiraphat1 <thiraphatnon14y@gmail.com>
2026-07-06 17:34:58 +0700

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,92 @@
+function IamName() {
+
+    document.getElementById("title").innerHTML =
+    "ธนชัย คงแถลง &#128526";
+
+    document.getElementById("friendImage").src =
+    "55.jpg";
+
+    document.getElementById("friendImage").width = 350;
+    document.getElementById("friendImage").height = 250;
+
+    document.getElementById("text1").innerHTML =
+    "ถึงพี่จะหรั่ง แต่พี่ก็ไม่ช้านะ";
+
+    document.getElementById("text2").innerHTML =
+    "ห่ะ, ป๋าแซม &#128509";
+}
+
+function Myfriend1() {
+
+    document.getElementById("title").innerHTML =
+    "ศุภเสกข์ พลขยัน &#128126";
+
+    document.getElementById("friendImage").src =
+    "66.jpg";
+
+    document.getElementById("friendImage").width = 350;
+    document.getElementById("friendImage").height = 250;
+
+    document.getElementById("text1").innerHTML =
+    "NO.1 เรื่องดิส เพราะพี่ไม่คิดจะสนใคร";
+
+    document.getElementById("text2").innerHTML =
+    "By ป๋าลุย &#128184";
+}
+
+function Myfriend2() {
+
+    document.getElementById("title").innerHTML =
+    "ธีรภัทร์ ดุลกลาง &#128081";
+
+    document.getElementById("friendImage").src =
+    "11.jpg";
+
+    document.getElementById("friendImage").width = 350;
+    document.getElementById("friendImage").height = 250;
+
+    document.getElementById("text1").innerHTML =
+    "ไม่แข่งกับใคร แข่งกับตัวเองก็พอ";
+
+    document.getElementById("text2").innerHTML =
+    "BY ป๋าโหน่ง &#128142";
+}
+
+function Myfriend3() {
+
+    document.getElementById("title").innerHTML =
+    "ศุภโชค แสงสิทธิ์ &#127992;";
+
+    document.getElementById("friendImage").src =
+    "33.jpg";
+
+    document.getElementById("friendImage").width = 350;
+    document.getElementById("friendImage").height = 250;
+
+    document.getElementById("text1").innerHTML =
+    "ถึงพี่จะสี่เก้า แต่ห้าเก้าพี่ก็ไม่หวั่น";
+
+    document.getElementById("text2").innerHTML =
+    "By ฟันดาบ &#128526";
+}
+
+function Myfriend4() {
+
+    document.getElementById("title").innerHTML =
+    "ศุภกฤต เผ่าพันธ์ &#128081";
+
+    document.getElementById("friendImage").src =
+    "44.jpg";
+
+    document.getElementById("friendImage").width = 350;
+    document.getElementById("friendImage").height = 250;
+
+    document.getElementById("text1").innerHTML =
+    "ถึงพี่จะเกย์ แต่พี่ไม่เคยเทใคร";
+
+    document.getElementById("text2").innerHTML =
+    "BY ป๋าเต49 &#128520";
+}
+
+
+window.onload = IamName;
